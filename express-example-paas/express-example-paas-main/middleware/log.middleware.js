a4f6b7b8405d0157180e09e818b0e443b2693387
DRLKH7 <darrelkhayruadityansyah@gmail.com>
2025-12-24 17:07:04 +0700

    initial commit

---
+++ express-example-paas/express-example-paas-main/middleware/log.middleware.js
@@ -0,0 +1,8 @@
+const logMiddleware = (req, res, next) => {
+  const now = new Date();
+  const timestamp = now.toISOString().replace('T', ' ').substring(0, 19);
+  console.log(`[${timestamp}] ${req.method} ${req.url}`);
+  next();
+};
+
+export default logMiddleware;
