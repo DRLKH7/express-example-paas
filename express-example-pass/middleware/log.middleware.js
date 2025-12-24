9e2669c27d58d11fc1514e45e49d54918f559ddf
Darrel Khayru A. <162407068+DRLKH7@users.noreply.github.com>
2025-12-24 17:54:17 +0700

    Delete express-example-pass directory

--- express-example-pass/middleware/log.middleware.js
+++
@@ -1,8 +0,0 @@
-const logMiddleware = (req, res, next) => {
-  const now = new Date();
-  const timestamp = now.toISOString().replace('T', ' ').substring(0, 19);
-  console.log(`[${timestamp}] ${req.method} ${req.url}`);
-  next();
-};
-
-export default logMiddleware;
